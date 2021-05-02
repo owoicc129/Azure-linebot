e19c81011adf98814787212fa4915f2a27bf5ebe
owoicc129 <owoicc129@gmail.com>
2021-05-02 15:41:36 +0800

    line_chat_bot

--- application.py
+++ application.py
@@ -1,11 +1,50 @@
-import flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    FlexSendMessage
+)
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+LINE_SECRET = "1416ba6268b14b2843a5d0972b5eaae7"
+LINE_TOKEN = "lQsLvMfbmHOXXexZevxqeFBA/kAB1s2/ch56Jutm0zLzFNxO8HYhx4elnsqxr2vh4MfPfCqww9w1P1p5LZYvu2MCA+FU6ruzNQHhS6oEFvzKzuFTHhUmNjLJLDQxjztq/IXsmZgIArNhG+k8xKwVlwdB04t89/1O/w1cDnyilFU="
+LINE_BOT = LineBotApi(LINE_TOKEN)
+HANDLER = WebhookHandler(LINE_SECRET)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Hello Flask!</h1>"
-
+@app.route("/callback", methods=["POST"])
+def callback():
+    # X-Line-Signature: 數位簽章
+    signature = request.headers["X-Line-Signature"]
+    print(signature)
+    body = request.get_data(as_text=True)
+    print(body)
+    try:
+        HANDLER.handle(body, signature)
+    except InvalidSignatureError:
+        print("Check the channel secret/access token.")
+        abort(400)
+    return "OK"
+    
+# message 可以針對收到的訊息種類
+@HANDLER.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    url_dict = {
+      "TIBAME":"https://www.tibame.com/coursegoodjob/traffic_cli", 
+      "HELP":"https://developers.line.biz/zh-hant/docs/messaging-api/"}
+# 將要發出去的文字變成TextSendMessage
+    try:
+        url = url_dict[event.message.text.upper()]
+        message = TextSendMessage(text=url)
+    except:
+        message = TextSendMessage(text=event.message.text)
+# 回覆訊息
+    LINE_BOT.reply_message(event.reply_token, message)
+    
 if __name__=='__main__':
     app.run()
+    
