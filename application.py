7745ba49995a6f3d29932d26b906db8aa16b2379
owoicc129 <owoicc129@gmail.com>
2021-05-03 15:44:09 +0800

    1010

--- application.py
+++ application.py
@@ -17,6 +17,7 @@ from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 import json
 import requests
+import re
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.face.models import TrainingStatusType
@@ -72,7 +73,74 @@ def azure_face_recognition(filename):
     return person.name
 
 
+def azure_ocr(url):
 
+    ocr_results = CV_CLIENT.read(url, raw=True)
+    operation_location_remote = \
+    ocr_results.headers["Operation-Location"]
+    operation_id = operation_location_remote.split("/")[-1]
+    status = ["notStarted", "running"]
+    while True:
+        get_handw_text_results = \
+        CV_CLIENT.get_read_result(operation_id)
+        if get_handw_text_results.status not in status:
+            break
+        time.sleep(1)
+
+    text = []
+    succeeded = OperationStatusCodes.succeeded
+    if get_handw_text_results.status == succeeded:
+        res = get_handw_text_results.\
+        analyze_result.read_results
+        for text_result in res:
+            for line in text_result.lines:
+                if len(line.text) <= 8:
+                    text.append(line.text)
+    # 利用 Regular Expresion (正規表示法) 針對台灣車牌的規則過濾
+    r = re.compile("[0-9A-Z]{2,4}[.-]{1}[0-9A-Z]{2,4}")
+    text = list(filter(r.match, text))
+    return text[0].replace(".", "-") if len(text) > 0 else ""
+
+
+def azure_object_detection(url, filename):
+    img = Image.open(filename)
+    draw = ImageDraw.Draw(img)
+    font_size = int(5e-2 * img.size[1])
+    fnt = ImageFont.truetype(
+      "static/TaipeiSansTCBeta-Regular.ttf", size=font_size)
+    object_detection = CV_CLIENT.detect_objects(url)
+    if len(object_detection.objects) > 0:
+        for obj in object_detection.objects:
+            left = obj.rectangle.x
+            top = obj.rectangle.y
+            right = obj.rectangle.x + obj.rectangle.w
+            bot = obj.rectangle.y + obj.rectangle.h
+            name = obj.object_property
+            confidence = obj.confidence
+
+            # 畫框並標上物件名稱與信心程度
+            draw.rectangle([left, top, right, bot], outline=(255, 0, 0), width=3)
+            draw.text([left, top + font_size],"{} {}".format(name, confidence),fill=(255, 0, 0),font=fnt)
+
+    # 把畫完的結果存檔，利用 imgur 把檔案轉成網路連結
+    img.save(filename)
+    image = IMGUR_CLIENT.image_upload(filename, "", "")
+    link = image["response"]["data"]["link"]
+    # 最後刪掉圖檔
+    os.remove(filename)
+    return link
+
+
+def azure_describe(url):
+    description_results = CV_CLIENT.describe_image(url)
+    output = ""
+    for caption in description_results.captions:
+        output += "'{}' with confidence {:.2f}% \n".format(
+            caption.text, caption.confidence * 100
+        )
+    return output
+
+    
 
 #--linebot-------------------------------
 
@@ -133,26 +201,27 @@ def handle_content_message(event):
     link = image["response"]["data"]["link"]
     
     
+    #[ok]
     name = azure_face_recognition(filename)
     if name != "": # 如果只有一張人臉，輸出人臉辨識結果
         now = datetime.now(timezone(timedelta(hours=8))).\
         strftime("%Y-%m-%d %H:%M") # 注意時區
         output = "{0}, {1}".format(name, now)
         
-    # else:
-    #     plate = azure_ocr(link)
-    #     link_ob = azure_object_detection(link, filename)
-    #     # 有車牌就輸出車牌
-    #     if len(plate) > 0:
-    #         output = "License Plate: {}".format(plate)
-    #     # 沒有車牌就就輸出影像描述的結果
-    #     else:
-    #         output = azure_describe(link)
-    #     link = link_ob
+    else:
+        plate = azure_ocr(link)
+        link_ob = azure_object_detection(link, filename)
+        # 有車牌就輸出車牌
+        if len(plate) > 0:
+            output = "License Plate: {}".format(plate)
+        # 沒有車牌就就輸出影像描述的結果
+        else:
+            output = azure_describe(link)
+        link = link_ob
     
     
     
-    # 回覆訊息[test get url ok!]
+    # 回覆訊息[ok!]
     LINE_BOT.reply_message(event.reply_token, TextSendMessage(text=output))
     
     
