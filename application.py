cb610376fc8c288a801bdbcf617f23f8b061f29f
owoicc129 <owoicc129@gmail.com>
2021-05-03 14:21:12 +0800

    16

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent,
+    ImageMessage,
     TextMessage,
     TextSendMessage,
     FlexSendMessage
